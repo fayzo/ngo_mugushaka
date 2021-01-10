172346db9bb031d101fed3d683fa2b76814dc61e
fayzo <shemafaysal@gmail.com>
2021-01-10 02:34:59 +0200

    sdf

---
+++ projctzs/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>jQuery Active Class</title>
+    <link href="style.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="ohdangitsweird.php">Eek!</a></li>
+      </ul>
+    </nav>
+    <h1>Contact</h1>
+  </body>
+</html>
